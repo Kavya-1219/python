8356e6821c3c35987763e327e37805e8bde257c7
Kavya-1219 <132338691+Kavya-1219@users.noreply.github.com>
2025-12-25 21:40:43 +0530

    Add files via upload

---
+++ leftTriangle.py
@@ -0,0 +1,3 @@
+n=int(input("enter num of rows:"))
+for i in range(1,n+1):
+    print(" " * (n-i) + "*" * i)
---
+++ lowerPyramid.py
@@ -0,0 +1,3 @@
+n=int(input("enter num of rows:"))
+for i in range(n,0,-1):
+    print(" " * (n-i)+" *" * i+ " " * (n-i))
